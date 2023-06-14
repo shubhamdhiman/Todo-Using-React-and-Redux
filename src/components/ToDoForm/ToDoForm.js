7f0231d134754b672803c0d5922d00346834da74
shubhamdhiman <shubhamdhiman00.sd@gmail.com>
2023-06-14 13:55:16 +0530

    Added dispatch

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./ToDoForm.css";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../redux/actions/todoActions";
 
 function ToDoForm({ onCreateTodo }) {
   const [todoText, setTodoText] = useState("");
-
+  const dispatch = useDispatch()
   const handleSubmit = (e) => {
     e.preventDefault();
-    onCreateTodo(todoText);
+    dispatch(addTodo(todoText))
+    // onCreateTodo(todoText);
     setTodoText("");
   };
 
